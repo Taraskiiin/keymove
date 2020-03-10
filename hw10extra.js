b910cacf5560fd48bbab96013c01cb7199be7480
taraskiiin <taraskravets95@gmail.com>
2020-03-10 02:02:49 +0200

    whitepoint

--- hw10extra.js
+++ hw10extra.js
@@ -3,29 +3,41 @@ let moveStepY = 0;
 let moveStepX = 0;
 const pointMoveDown = () => {
     moveStepY += 15;
-    player.style.top = moveStepY + "px";
+    if (moveStepY > -15 && moveStepY < 435) {
+        player.style.top = moveStepY + "px";
+    }
 }
 const pointMoveUp = () => {
     moveStepY -= 15;
-    player.style.top = moveStepY + "px";
+    if (moveStepY > -15 && moveStepY < 435) {
+        player.style.top = moveStepY + "px";
+    }
 }
 const pointMoveLeft = () => {
     moveStepX -= 15;
-    player.style.left = moveStepX + "px";
+    if (moveStepX > -15 && moveStepX < 985) {
+        player.style.left = moveStepX + "px";
+    }
 }
-function pointMoveRight(){
+function pointMoveRight() {
     moveStepX += 15;
-    player.style.left = moveStepX + "px";
+    if (moveStepX > -15 && moveStepX < 985) {
+        player.style.left = moveStepX + "px";
+    }
 }
 document.addEventListener('keydown', function (event) {
     switch (event.keyCode) {
         case 40:
             pointMoveDown();
+            break;
         case 38:
             pointMoveUp();
+            break;
         case 39:
             pointMoveRight();
+            break;
         case 37:
             pointMoveLeft();
-    }       
+            break;
+    }
 });
